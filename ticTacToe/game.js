62b25273e35f048bc5b499f85d26b66929510d45
Al <shimoalmail@gmail.com>
2016-09-27 18:00:14 +0900

    first commit

---
+++ ticTacToe/game.js
@@ -0,0 +1,31 @@
+var board = [[1, 2, 3],
+						 [4, 5, 6],
+						 [7, 8, 9]];
+
+var winningCombos = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9],
+	[1,4,7],
+	[2,5,8],
+	[3,6,9],
+	[1,5,9],
+	[3,5,7]
+];
+
+var player1 = {
+	mark: "X",
+	wins: []
+};
+var player2 = {
+	mark: "O",
+	wins: []
+};
+
+function gameWon(player){
+}
+
+function startGame(){
+	var turn = 0;
+	
+}
